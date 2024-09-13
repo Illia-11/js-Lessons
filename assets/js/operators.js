3584938d9f419c21f897909cd02d41befebb32d9
Illia Piddubnyi <illya2008zp@gmail.com>
2024-09-13 20:13:35 +0300

    Add basic math operations

--- assets/js/operators.js
+++ assets/js/operators.js
@@ -4,8 +4,34 @@
 typeof 10;
 
 let data = 'test';
-
 console.log(typeof data);
 
 let bug = null;
 console.log(typeof bug); // object
+
+// Математичні оператори
+// +, -, *, /,
+console.log('2 + 2');
+console.log(2 + 2);
+
+console.log('9 - 2');
+console.log(9 - 2);
+
+console.log('10 * 2');
+console.log(10 * 2);
+
+console.log('4 / 2');
+console.log(4 / 2);
+
+// зведення в ступінь
+console.log('3 ** 3');
+console.log(3 ** 3);
+
+console.log('(-3) ** 3');
+console.log((-3) ** 3);
+
+// залишок від ділення
+console.log('10 % 3');
+console.log(10 % 3);
+console.log(10 % -3);
+console.log(-10 % 3);
