33cacea3c23a2a1fa10dd07dbebf523883611c4d
Illia Piddubnyi <illya2008zp@gmail.com>
2024-09-15 13:25:44 +0300

    Add homework solution

---
+++ assets/js/task.js
@@ -0,0 +1,23 @@
+// task1 
+let num1 = 5;
+let num2 = 10;
+let numSum = (num1 + num2);
+console.log(numSum) 
+
+// task2
+let numSum2 = (numSum - 50);
+console.log(numSum2);
+
+// task3
+let numSum3 = (numSum2 * numSum);
+console.log(numSum3);
+
+// task4
+let numSum4 = (numSum3 / 2.687)
+console.log(numSum4);
+let numSum5 = (numSum3 % 2.687);
+console.log(numSum5);
+
+// task5
+let numSum6 = (numSum.toString());
+console.log(numSum6);
