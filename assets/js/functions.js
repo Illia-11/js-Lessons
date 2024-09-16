74625e4eddcce1e16f779e5d9bcdd029f7c96085
Illia Piddubnyi <illya2008zp@gmail.com>
2024-09-16 20:27:54 +0300

    Add prompt function example

---
+++ assets/js/functions.js
@@ -0,0 +1,11 @@
+// Функції - блоки коду які можна повтороно використовувати
+
+/* 
+  Параметри функції - речі які передаємо всередину як додаткову інформацію
+*/
+let text = 'text';
+console.log(text, 123);
+
+const userName = prompt('Enter your name');
+const greetingText = 'Hi, ' + userName + '!';
+alert(greetingText);
