6f85efd8d801b985c80b95c17538e6765c96114e
Illia Piddubnyi <illya2008zp@gmail.com>
2024-09-17 18:15:09 +0300

    Add function create example

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,9 +3,44 @@
 /* 
   Параметри функції - речі які передаємо всередину як додаткову інформацію
 */
-let text = 'text';
-console.log(text, 123);
+// let text = 'text';
+// console.log(text, 123);
 
-const userName = prompt('Enter your name');
-const greetingText = 'Hi, ' + userName + '!';
-alert(greetingText);
+// const userName = prompt('Enter your name');
+// const greetingText = 'Hi, ' + userName + '!';
+// alert(greetingText);
+
+// const num1 = prompt("Enter first number: ");
+// const num2 = prompt("Enter second number: ")
+// alert(+num1 + +num2);
+
+// const num1 = +prompt("Enter first number: ");
+// const num2 = +prompt("Enter second number: ")
+// alert(num1 + num2);
+
+// const input1 = prompt("Enter first number: ");
+// const input2 = prompt("Enter second number: ")
+// const num1 = +input1;
+// const num2 = +input2;
+// alert(num1 + num2);
+
+// const result = prompt ();
+// console.log(result);
+
+
+// Створення функції
+
+// functional expression (функціональний вираз)
+const myFirstFunction = function() {
+  // тіло функції - блок коду який буде виконано коли функція запуститься
+  console.log('Hello from first function');
+  console.log('You can do anything you want here');
+}
+myFirstFunction();
+
+// const mySecondFunction = function() {
+//   console.log('Hello, world!')
+//   console.log("I'm here");
+// }
+// mySecondFunction();
+
