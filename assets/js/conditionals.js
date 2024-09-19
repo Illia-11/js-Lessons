13c4c078ad412b853d7e74f43cc57d9aeba923aa
Illia Piddubnyi <illya2008zp@gmail.com>
2024-09-19 20:34:18 +0300

    Add ternars example

--- assets/js/conditionals.js
+++ assets/js/conditionals.js
@@ -22,14 +22,77 @@ if(text) {
   console.log('блок коду 3');
 }
 
-let number = +prompt('Enter number');
+// let number = +prompt('Enter number');
 
-if(number > 0) {
-  console.log('Number is positive');
-} else if(number < 0) {
-  console.log('Number is negative');
-} else if(number === 0) {
-  console.log('This is zero');
-} else {
-  console.log('No-no-no, mr. fish!');
+// if(number > 0) {
+//   console.log('Number is positive');
+// } else if(number < 0) {
+//   console.log('Number is negative');
+// } else if(number === 0) {
+//   console.log('This is zero');
+// } else {
+//   console.log('No-no-no, mr. fish!');
+// }
+
+
+// тернарний оператор
+// це спеціальний оператор який може замінити конструкцію if() {} else{}
+
+/*
+  if(умова) {
+  // action 1
+  // } else {
+  // action 2
 }
+
+  умова ? action 1 : action 2;
+*/
+
+// const userName = prompt('Enter your name');
+
+// if(userName) {
+//   // action 1
+//   alert('Hello, ' + userName);
+// } else {
+//   // action 2
+//   alert('Hello, user');
+// }
+userName ? alert('Hello, ' + userName) : alert('Hello, user');
+
+
+const getUserPrompt = function() {
+  const userPrompt = prompt('Enter smt');
+
+  // if(userPrompt) {
+  //   return userPrompt;
+  // } else {
+  //   return 'User enter nothing';
+  // }
+
+  return userPrompt ? userPrompt : 'user enter nothing';
+}
+const res1 = getUserPrompt();
+console.log(res1);
+
+
+// const userName = prompt('Enter your name');
+// let greetinMessage;
+
+// if(userName) {
+//   greetinMessage = 'Hi, ' + userName;
+// } else {
+//   greetinMessage = 'Hi, user';
+// }
+// greetinMessage = userName ? 'Hi, ' + userName : 'Hi, user';
+// const greetinMessage = userName ? 'Hi, ' + userName : 'Hi, user';
+
+// alert(greetinMessage);
+
+const userName = prompt('Enter your name');
+
+if(userName) {
+  alert('Hello, ' + userName);
+} else {
+  alert('Hello, user');
+}
+alert( userName ? ('Hello, ' + userName) : ('Hello, user'));
