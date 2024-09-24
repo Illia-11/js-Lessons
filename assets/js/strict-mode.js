b76064ac0b62dfd50bea2518376eaf4f19fec3f6
Illia Piddubnyi <illya2008zp@gmail.com>
2024-09-24 20:19:53 +0300

    Add strict model usage example

---
+++ assets/js/strict-mode.js
@@ -0,0 +1,32 @@
+"use strict"; // вмикає суворий режим для файла
+// суворий режим викоання JS
+
+// let piblicInfo = 'everyone look'
+// if(true) {
+//   var secret = 'dont look';
+
+//   console.log(secret);
+//   console.log(piblicInfo);
+// }
+// console.log(piblicInfo);
+// console.log(secret);
+
+
+function thisTest() {
+  // console.log(this);
+
+  // console.log(this.fetch);
+
+  // this.fetch = 'vhfjekbvhdcv,ds'
+
+  // console.log(this.fetch);
+
+  // this.alert = 10;
+}
+// alert('test');
+// thisTest();
+
+userName = 'Blablabla';
+
+console.log(userName);
+console.log(window);
