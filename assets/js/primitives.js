2782ae507a1636b28d432761a4f172c3ca389156
Illia Piddubnyi <illya2008zp@gmail.com>
2024-10-04 18:57:15 +0300

    Add number primitive method examples

--- assets/js/primitives.js
+++ assets/js/primitives.js
@@ -101,6 +101,7 @@ function getRandomArbitrary(min, max) {
 
 // Методи примітивів
 
+// Boolean
 console.log(true);
 console.log(true.toString());
 
@@ -108,3 +109,15 @@ const boolValue = true;
 
 console.log(true.toString());
 console.log(boolValue.toString());
+
+// Number
+const num1 = 15.5;
+console.log(num1.toExponential()); // '1.55e+1'
+console.log(num1.toExponential(1)); // '1.6e+1'
+
+console.log(num1.toFixed()); // '16'
+console.log(num1.toFixed(1)); // '15.5'
+
+console.log(num1.toPrecision()); // '1.55'
+console.log(num1.toPrecision(1)); // '2e+1'
+
