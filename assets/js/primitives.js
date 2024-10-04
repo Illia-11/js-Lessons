d20f28ab9ee613dc24fbe4e58a37243f5a911233
Illia Piddubnyi <illya2008zp@gmail.com>
2024-10-04 20:02:40 +0300

    Add String examples

--- assets/js/primitives.js
+++ assets/js/primitives.js
@@ -121,3 +121,36 @@ console.log(num1.toFixed(1)); // '15.5'
 console.log(num1.toPrecision()); // '1.55'
 console.log(num1.toPrecision(1)); // '2e+1'
 
+// String
+const objStr = new String('This is test string');
+
+const str = 'This is test string';
+
+console.log(str.length);
+
+console.log(str[2]);
+
+// str[2] = 'I'; // error
+
+// charAt - повертає символ на вказаному індексу в рядку
+const symbol1 = str.charAt(2); // 'i'
+
+// charCodeAt - повертає Юнікодівське значення символу на вказаному індексу в рядку
+const unicodeNum1 = str.charCodeAt(2); // 105
+
+// concat - повертає новий рядок, в якому до старого приєднуються (конкатинуються) аргументи конкату (зазвичай рядки)
+// str + ' CONCAT ' + 10 + ' !!!';
+const concatStr = str.concat(' CONCAT ', 1235, ' !!!');
+
+// includes - відповідає чи входить рядок-аргумент у склад рядка у якого викликаємо метод
+const includesStr = str.includes('t'); // true
+
+// indexOf - повертає індекс першого входження рядка-аргумента у рядку, у якого метод викликали
+const indexOfStr = str.indexOf('t'); // 8
+const indexOfStr2 = str.indexOf('bdsbdf'); // -1
+const indexOfStr3 = str.indexOf('t', 9); // 11
+
+// slice - робить копію частини рядка
+const sliceStr = str.slice(2, 13); // "is is test "
+const sliceStr1 = str.slice(2, -4); // "is is test st"
+
