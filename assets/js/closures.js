8f4a488adb1ac5ff08e890ce9ef5c469813ef6e6
Illia Piddubnyi <illya2008zp@gmail.com>
2024-10-10 20:06:37 +0300

    Add basic closures example

---
+++ assets/js/closures.js
@@ -0,0 +1,49 @@
+let data = 'letter';
+
+function log1() {
+  console.log(`log1: ${data}`);
+};
+
+log1(); // letter
+
+function log2() {
+  let data = 151;
+  console.log(`log2: ${data}`);
+};
+
+log2(); // 151
+
+function log3() {
+  let data = NaN;
+
+  console.log(`log3 outer: ${data}`); // NaN
+
+  log1(); // letter
+
+  if(true) {
+    let data = true;
+    console.log(`log3 outer: ${data}`); // true
+  };
+};
+
+log3();
+
+
+// Лексичне оточення функції - спеціальний обʼєкт, який містить всі змінні, які функція бачить
+// розраховується на момент створення функції
+
+function log4 () {
+  let data = 'TOP SECRET'; // змінна знаходиться у замиканні
+
+  function innerlog() {
+    console.log(data); // доступ до data буде навіть коли log4 завершить роботу
+  };
+
+  // innerlog();
+
+  return innerlog;
+};
+
+const innerLog1 = log4();
+
+innerLog1();
