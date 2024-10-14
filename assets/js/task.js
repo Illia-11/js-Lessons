ece015138d3d96177ec461dea58ec65b026ab617
Illia Piddubnyi <illya2008zp@gmail.com>
2024-10-14 17:48:22 +0300

    Add some recursions task solution

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,23 +1,71 @@
 // task1 
-let num1 = 5;
-let num2 = 10;
-let numSum = num1 + num2;
-console.log(numSum) 
+// let num1 = 5;
+// let num2 = 10;
+// let numSum = num1 + num2;
+// console.log(numSum) 
 
-// task2
-let minus = numSum - 50;
-console.log(minus);
+// // task2
+// let minus = numSum - 50;
+// console.log(minus);
 
-// task3
-let mult = minus * numSum;
-console.log(mult);
+// // task3
+// let mult = minus * numSum;
+// console.log(mult);
 
-// task4
-let div = mult / 2.687;
-console.log(div);
-let rem = mult % 2.687;
-console.log(rem);
+// // task4
+// let div = mult / 2.687;
+// console.log(div);
+// let rem = mult % 2.687;
+// console.log(rem);
 
-// task5
-let toStr = '' + numSum;
-console.log(toStr);
+// // task5
+// let toStr = '' + numSum;
+// console.log(toStr);
+
+console.log('task 1');
+
+function factorial (n) {
+  if (n === 0 ) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  };
+};
+console.log(factorial(3));
+
+
+console.log('task 2');
+
+function sumTo (n) {
+  if (n == 1) {
+    return 1;
+  };
+
+  return n + sumTo(n - 1);
+};
+console.log(sumTo(2));
+
+
+console.log('task 3');
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+function printList(list) {
+  console.log(list.value);
+
+  if (list.next) {
+    console.log(list.next);
+  };
+};
+console.log(printList(list));
