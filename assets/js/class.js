30ff0ae8dfd5f3e80af8056e36f0aa92c596012a
Illia Piddubnyi <illya2008zp@gmail.com>
2024-10-14 19:46:28 +0300

    Add Product task solution

--- assets/js/class.js
+++ assets/js/class.js
@@ -81,3 +81,41 @@ const house4 = new newHouse (1, 2, 'addres1', 4000);
 
 // створення екземпляра без new завжди викликає помилку
 // const house5 = newHouse (1, 2, 'addres1', 4000); // Error
+
+// class task
+
+class Product {
+  constructor (name, price, amount, isForAdult) {
+    this.name = name;
+    this.price = price;
+    this.amount = amount;
+    this.isForAdult = isForAdult;
+  };
+
+  getPriceOfAllProduct () {
+    // return `Загальна вартість всіх одиниць товару '${this.name}' = ${this.price * this.amount} грн`; 
+    // якщо треба повернути лише число або булеве значення - краще вказати тільки його
+
+    return this.price * this.amount;
+  };
+};
+
+const product1 = new Product('ice cream', 40, 10, false);
+const product2 = new Product('water', 20, 30, false);
+const product3 = new Product('beer', 100, 5, true);
+
+const owner1 = {
+  name: 'Owner 1',
+  email: 'owner1@gmail.com',
+  balance: 5000
+};
+
+// alert(`Загальна вартість всіх одиниць товару '${product1.name}' = ${product1.getPriceOfAllProduct()} грн`);
+
+// 
+// if(owner1.balance >= (product1.price * product1.amount)) {
+if(owner1.balance >= product1.getPriceOfAllProduct()) {
+  console.log('Owner 1 can buy all ice cream');
+} else {
+  console.log('Owner 1 can not buy all ice cream');
+};
